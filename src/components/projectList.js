32800ce1421344af4154c0d3bf283c4f616bce40
Guilherme Cabral <guilhermerocabral@hotmail.com>
2022-03-08 00:49:08 -0300

    Added link in weatherAPI project.

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -13,7 +13,7 @@ const ProjectList = [
   {
     name: "WaetherAPI",
     info: "Neste projeto o usuario informa uma cidade e o sistema apresenta na tela a temperatura e uma imagem do local, ambas chamadas de diferentes API's.",
-    link: "",
+    link: "https://weatherapi-gui.web.app",
     repository: "https://github.com/GuiCabral74/WeatherApi",
     img: WaetherAPI,
     alt: "WaetherAPI",
